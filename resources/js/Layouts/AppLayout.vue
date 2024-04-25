21dd5c117b5fbe75b0a45c2dd7ba8ebd545f5d1e
vimuths123 <vimuths@yahoo.com>
2024-04-25 14:49:35 +0530

    New changes

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,14 +1,12 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import { Head, Link, router, usePage } from '@inertiajs/vue3';
+import { ref } from 'vue';
+import { Head, Link, router } from '@inertiajs/vue3';
 import ApplicationMark from '@/Components/ApplicationMark.vue';
 import Banner from '@/Components/Banner.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { BellIcon } from '@heroicons/vue/24/solid'
-
 
 defineProps({
     title: String,
@@ -27,32 +25,6 @@ const switchToTeam = (team) => {
 const logout = () => {
     router.post(route('logout'));
 };
-;
-
-const notificationCount = ref(0);
-const notifications = ref([]);
-const newNotification = ref({});
-
-window.Echo.private("notifications." + usePage().props.auth.user.id).listen("NotificationCreated", (e) => {
-    notificationCount.value++;
-    newNotification.value = {
-        id: e.notification.id,
-        title: e.notification.title,
-        body: e.notification.body
-    }
-    notifications.value.unshift(newNotification.value);
-});
-
-onMounted(() => {
-    axios.get('/get_notifications')
-        .then(response => {
-            notifications.value = response.data;
-            notificationCount.value = response.data.length;
-        })
-        .catch(error => {
-            console.error('Error fetching notifications:', error);
-        });
-});
 </script>
 
 <template>
@@ -83,31 +55,59 @@ onMounted(() => {
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
-                            <div class="relative inline-block cursor-pointer">
-                                 <Dropdown align="right" width="96">
+                            <div class="ms-3 relative">
+                                <!-- Teams Dropdown -->
+                                <Dropdown v-if="$page.props.jetstream.hasTeamFeatures" align="right" width="60">
                                     <template #trigger>
-                                        <BellIcon class="h-7 w-7 text-gray-600" />
+                                        <span class="inline-flex rounded-md">
+                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                                {{ $page.props.auth.user.current_team.name }}
 
-                                        <span v-if="notificationCount > 0" class="absolute bottom-3 left-3 flex items-center justify-center h-5 w-5 rounded-full bg-red-600 text-white text-xs">
-                                            {{ notificationCount }}
+                                                <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                                </svg>
+                                            </button>
                                         </span>
                                     </template>
 
                                     <template #content>
-                                        <!-- Account Management -->
-                                        <div class="block px-4 py-2 text-xs text-gray-400">
-                                            Notifications
-                                        </div>
+                                        <div class="w-60">
+                                            <!-- Team Management -->
+                                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                                Manage Team
+                                            </div>
 
-                                        <div class="border-t border-gray-200" />
-
-                                        <div v-for="(notification, index) in notifications" :key="index">
-                                            <DropdownLink :href="route('click_notification', notification.id)">
-                                                <div class="block text-xs">{{ notification.title }}</div>
-                                                <div>{{ notification.body }}</div>
+                                            <!-- Team Settings -->
+                                            <DropdownLink :href="route('teams.show', $page.props.auth.user.current_team)">
+                                                Team Settings
                                             </DropdownLink>
 
-                                            <div class="border-t border-gray-200" />
+                                            <DropdownLink v-if="$page.props.jetstream.canCreateTeams" :href="route('teams.create')">
+                                                Create New Team
+                                            </DropdownLink>
+
+                                            <!-- Team Switcher -->
+                                            <template v-if="$page.props.auth.user.all_teams.length > 1">
+                                                <div class="border-t border-gray-200" />
+
+                                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                                    Switch Teams
+                                                </div>
+
+                                                <template v-for="team in $page.props.auth.user.all_teams" :key="team.id">
+                                                    <form @submit.prevent="switchToTeam(team)">
+                                                        <DropdownLink as="button">
+                                                            <div class="flex items-center">
+                                                                <svg v-if="team.id == $page.props.auth.user.current_team_id" class="me-2 h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                                </svg>
+
+                                                                <div>{{ team.name }}</div>
+                                                            </div>
+                                                        </DropdownLink>
+                                                    </form>
+                                                </template>
+                                            </template>
                                         </div>
                                     </template>
                                 </Dropdown>
